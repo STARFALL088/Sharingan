7cebe41a2756cc1fb83edef7a411ac511853babc
STARFALL088 <164139629+STARFALL088@users.noreply.github.com>
2025-12-10 12:20:22 +0600

    Add CLI argumrnt for handles and rating range

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+import argparse
 import requests
 import json
 ans={}
-LOW_RATING=1500
-HIGH_RATING=1700
 
+
+parser=argparse.ArgumentParser(
+    description="Here we go again"
+)
+
+
+parser.add_argument("--max",type=int,default=1700,help="Max rating")
+parser.add_argument("--min",type=int,default=1400,help="Min rating")
+
+parser.add_argument("--myID",type=str,default="SHADOW088 copycat69",help="My IDs")
+parser.add_argument("--target",type=str,default="daud04 iloveoru being_mysterious",help="My IDs")
+args=parser.parse_args()
+#print(args.myID)
+LOW_RATING=args.min
+HIGH_RATING=args.max
+MAX_TIME_SOLVED=0
 url = "https://codeforces.com/api/user.status"
 handle=[
-    "daud04",
-    "iloveoru",
-    "being_mysterious"
+    list(args.target.split())
 ]
 
 my_handle=[
-    "SHADOW088",
-    "copycat69"
+    # "SHADOW088",
+    # "copycat69"
+    list(args.myID.split())
 ]
-
+#print(my_handle)
+#print(handle)
 def get_data(h):
     params = {
 
@@ -43,8 +58,11 @@ def get_problem_set(h):
                 prob['index'],
                 prob['name'],
                 prob['rating'],
+                prob['contestId']
             )
             ans[z]=ans.get(z,0)+1
+            global MAX_TIME_SOLVED
+            MAX_TIME_SOLVED=max(ans[z],MAX_TIME_SOLVED) 
 
 def remove_my_solved(h):
     data=get_data(h)
@@ -59,7 +77,8 @@ def remove_my_solved(h):
             z=(
                 prob['index'],
                 prob['name'],
-                prob['rating']
+                prob['rating'],
+                prob['contestId']
             )
             if(z not in ans):
                 continue
@@ -70,5 +89,7 @@ for h in handle:
 for h in my_handle:
     remove_my_solved(h)
 for key,val in ans.items():
-    if val>0:
-        print(f"{key} solve_count: {val}")
+    if val==MAX_TIME_SOLVED:
+        #print(f"{key} solve_count: {val}")
+        print(f"https://codeforces.com/problemset/problem/{key[3]}/{key[0]}")
+
